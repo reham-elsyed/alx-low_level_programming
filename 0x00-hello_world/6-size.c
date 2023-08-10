5a5a0c7e4416658342c466d0f53e774f40d0fe74
reham-elsyed <rehamshipl666@gmail.com>
2023-08-10 12:21:31 -0700

    type od data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ long int d;
 long long int a;
 float f;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
