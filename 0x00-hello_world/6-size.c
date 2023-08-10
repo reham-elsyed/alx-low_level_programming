d575f9ab7208ea24e00b34a894fb2ac6b2793c1c
reham-elsyed <rehamshipl666@gmail.com>
2023-08-10 11:30:56 -0700

    type of data

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - A program that prints the size of various computer types
+* Return 0 (Success)
+*/
+int main(void)
+{
+char c;
+int i;
+long int d;
+long long int a;
+float f;
+
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+return (0);
+}
