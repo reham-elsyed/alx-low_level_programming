68fa36088dc4f79105dde816b0c1b661abbc2be1
reham-elsyed <rehamshipl666@gmail.com>
2023-08-10 11:40:53 -0700

    bits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,11 @@ long int d;
 long long int a;
 float f;
 
+printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
