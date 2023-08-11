adbf1a9ba10b72cad576f7540ecf2ea602136818
reham-elsyed <rehamshipl666@gmail.com>
2023-08-11 06:11:28 -0700

    if else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 
 
-/*
+/**
 * main - A function that prints a random number
 *
 * Return: 0(Success)
@@ -18,15 +18,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d :is positive", n);
+printf("%d is positive", n);
 }
 else if (n < 0)
 {
-printf("%d :is negative", n);
+printf("%d is negative", n);
 }
 else
 {
-printf("%d :is zero", n);
+printf("%d is zero", n);
 }
 return (0);
 }
