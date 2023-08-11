aacba44260c41551159b00e8febad934a49c9a89
reham-elsyed <rehamshipl666@gmail.com>
2023-08-11 03:11:50 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print string without using printf or puts
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return 0; }
