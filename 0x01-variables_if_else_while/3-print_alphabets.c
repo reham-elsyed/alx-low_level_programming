964e3438ef170f662d712d6534fa6d92163e828a
reham-elsyed <rehamshipl666@gmail.com>
2023-08-11 07:25:28 -0700

    alpha
    dfdgd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+* main - a program that prints the alphabet in lowercase
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar('a' + i);
+putchar('A' + i);
+}
+putchar('\n');
+return (0);
+}
