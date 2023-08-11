41077d106c3d2a4e3a25d323556816d456193583
reham-elsyed <rehamshipl666@gmail.com>
2023-08-11 03:16:01 -0700

    quote git commit -mquote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - print string without using printf or puts
  * Return: 1 (Success)
@@ -6,5 +7,5 @@
 
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return 0; }
