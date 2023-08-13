b701a79173625aad5d70e3ce67c06ada5e7d0eef
reham-elsyed <rehamshipl666@gmail.com>
2023-08-13 07:58:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-const char *str = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+const char *str =
+"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i;
 
 for (i = 0; str[i] != '\0'; i++)
 {
 putchar(str[i]);
 }
+putchar('\n');
 return (1);
 }
