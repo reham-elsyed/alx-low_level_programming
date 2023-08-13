3bf383c6a00485b5eea582a8e337d6d49caaaa72
reham-elsyed <rehamshipl666@gmail.com>
2023-08-13 07:48:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,14 @@
  * main - print string without using printf or puts
  * Return: 1 (Success)
  */
-
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return 0; }
+const char *str = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+return (1);
+}
