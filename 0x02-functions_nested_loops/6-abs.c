1acab5210679597ff0859eed449387f00b2f4f61
reham-elsyed <rehamshipl666@gmail.com>
2023-08-15 14:50:03 -0700

    check absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs - calculate the absolute value of an intger
+ *@n: input value
+ *Return: absolute value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
