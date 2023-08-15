4022be569115ef8aa6b41985d5a5bdfbeba4993c
reham-elsyed <rehamshipl666@gmail.com>
2023-08-15 16:38:46 -0700

    times tsble

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table -prints result of times table
+ *
+ */
+void times_table(void)
+{
+int i, j;
+i = 0;
+while (i <= 9)
+{
+j = 0;
+while (j <= 9)
+{
+int result = i *j;
+
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
+if (j != 9)
+{
+_putchar(',');
+_putchar('.');
+_putchar('.');
+}
+j++
+}
+putchar('\n');
+i++
+}
+}
