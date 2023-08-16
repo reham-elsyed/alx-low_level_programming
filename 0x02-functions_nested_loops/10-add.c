8a61360f4f54568ea691270e4db6eda0655d192d
reham-elsyed <rehamshipl666@gmail.com>
2023-08-15 17:45:14 -0700

    sum of 2 digit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two ingers
+ * @i: input number
+ * @j: input number
+ * Return: sum success
+ */
+int add(int i, int j)
+{
+int result;
+result = i + j;
+return (result);
+}
