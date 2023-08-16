51ac53b04c6d62fbd1b59d05b730587450b0216b
reham-elsyed <rehamshipl666@gmail.com>
2023-08-16 09:16:16 -0700

    times table with input

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - function to print time table with certain input
+ * @n: input to the function
+ */
+void print_times_table(int n)
+{
+int i, j;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int result = i * j;
+
+if (j == 0)
+{
+_putchar('0' + reuslt);
+}
+if (result < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + result / 10);
+}
+else if (result >= 10)
+{
+putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
+}
+}
+_putchar('\n');
+}
+}
+
+
