3b0058f6a004dfcecf4916a60eef7f20a9e140ff
reham-elsyed <rehamshipl666@gmail.com>
2023-08-17 08:12:10 -0700

    multipblication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiply two intger
+ * @a: first input
+ * @b: second input
+ *Return: 0 success
+ */
+int mul(int a, int b)
+{
+int mult = a * b;
+return (mult);
+}
