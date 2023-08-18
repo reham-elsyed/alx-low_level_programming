a7f1cbe281613c45050d5f35219eb9b213d3539c
reham-elsyed <rehamshipl666@gmail.com>
2023-08-18 16:47:37 -0700

    diagonal shape

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,13 @@ void print_diagonal(int n)
 {
 int i, j;
 
-for (i = 0; i < n; i++)
-{
 if (n <= 0)
 {
 _putchar('\n');
 }
-else if (n > 0)
+for (i = 0; i < n; i++)
+{
+if (n > 0)
 {
 for (j = 0; j < i; j++)
 {
