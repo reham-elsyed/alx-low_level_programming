26687f92608c71f21f692e83f3a07c9d5be92ea3
reham-elsyed <rehamshipl666@gmail.com>
2023-08-18 16:14:37 -0700

    prints diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal shape
+ * @n: in input number
+ */
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else if (n > 1)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
