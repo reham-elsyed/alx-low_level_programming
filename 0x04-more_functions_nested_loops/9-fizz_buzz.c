aca07568d93a96ce3b42b2e02907b45c57bc22a6
reham-elsyed <rehamshipl666@gmail.com>
2023-08-18 18:02:04 -0700

    buzzfizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - function print numbers from 1-100
+ * replace 3multibles with fiss and 5multibles with buzz
+ *
+ * Return: 0 sucess
+ */
+int main(void)
+{
+int i;
+for (i = 1; i < 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+(printf("FizzBuzz ")
+}
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{printf("%d ", i);
+}
+}
+}
