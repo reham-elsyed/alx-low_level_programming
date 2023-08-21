e8edc40bd0abf20c097fd7b4981f6c16b08c35c4
reham-elsyed <rehamshipl666@gmail.com>
2023-08-20 23:45:10 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rese_to_98 - a function to create a var and pointer
+ * @n: the value of the dereferencing pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps values of two vars
+ *
