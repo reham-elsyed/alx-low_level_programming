fa8bb4c4de5ae87cadab59b077fc07652edbbf1e
reham-elsyed <rehamshipl666@gmail.com>
2023-08-21 00:03:51 -0700

    swap values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,4 +2,14 @@
 
 /**
  * swap_int - function that swaps values of two vars
- *
+ *@a: value of first int
+ *@b: value of second int
+ *Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
