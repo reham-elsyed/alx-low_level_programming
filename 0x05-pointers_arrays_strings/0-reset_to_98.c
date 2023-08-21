b97e3ca94a56f0ff45c13814414ed86407b9707e
reham-elsyed <rehamshipl666@gmail.com>
2023-08-20 23:50:38 -0700

    pointer and array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * rese_to_98 - a function to create a var and pointer
- * @n: the value of the dereferencing pointer
- * Return: nothing
- */
+*reset_to_98 - a function to create a var and pointer
+*@n: the value of the dereferencing pointer
+*Return: nothing
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
