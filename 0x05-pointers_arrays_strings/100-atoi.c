fd793955cb32417a503aa4839dbdaa835c97a755
reham-elsyed <rehamshipl666@gmail.com>
2023-08-22 05:57:58 -0700

    print numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,23 @@
 #include "main.h"
 
 /**
- * _atoi - a function to convert string to intger
- * @s:pointer to string
- * Return: 0 if no numbers,value if exist
- */
-#include "main.h"
-
-/**
  * _atoi - a function to convert string to integer
  * @s: pointer to string
  * Return: 0 if no numbers, value if exists
  */
 int _atoi(char *s)
 {
-    unsigned int digit = 0;
-    int sign = 1;
-
-    while (*s != '\0') {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-            digit = (digit * 10) + (*s - '0');
-        else if (digit > 0)
-            break;
-
-        s++;
-    }
-
-    return (digit * sign);
+unsigned int digit = 0;
+int sign = 1;
+while (*s != '\0')
+{
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+digit = (digit * 10) + (*s - '0');
+else if (digit > 0)
+break;
+s++;
+}
+return (digit * sign);
 }
