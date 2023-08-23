6a9109e7c51a312abe8fcba62c63794aa8897f49
reham-elsyed <rehamshipl666@gmail.com>
2023-08-23 09:35:03 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,12 +5,13 @@
  *@n: number of element
  *Return: nothing
  */
-void reverse_array(int *a,int n)
+void reverse_array(int *a, int n)
 {
-int i;
-for (i = (n - 1); i >= 0; i--)
+int i, temp;
+for (i = 0; i < n / 2; i++)
 {
-_putchar(a[i]);
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
 }
-_putchar('\n');
 }
