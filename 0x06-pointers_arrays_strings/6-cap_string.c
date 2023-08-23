4ef9ee50196700f9a4b44fa9744beef76bb7417b
reham-elsyed <rehamshipl666@gmail.com>
2023-08-23 12:56:45 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,26 +9,24 @@ char *cap_string(char *str)
 {
 char del[] = " \t\n.;,!?\"(){}";
 int i, j;
-int is_delimit = 0;
+int is_delimiter = 0;
 
-for (i = 0; str[i] !='\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
-if (i == 0)
-{
-if(str[i] >= 'a' && str[i] <= 'z')
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - 32;
 }
-}
+is_delimiter = 0;
 for (j = 0; del[j] != '\0'; j++)
 {
 if (str[i] == del[j])
 {
-is_delimit = 1;
+is_delimiter = 1;
 break;
 }
 }
-if (is_delimit && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+if (is_delimiter && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 {
 str[i + 1] = str[i + 1] - 32;
 }
