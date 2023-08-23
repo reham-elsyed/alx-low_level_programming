eee4f06e5068f791d2e0cbad89c85ef6a23f814d
reham-elsyed <rehamshipl666@gmail.com>
2023-08-23 13:16:36 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @str: input value
+ * Return: str
+ */
+char *leet(char *str)
+{
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s1[] = "4433007711";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (str[i] == s1[j])
+{
+str[i] = s2[j];
+}
+}
+}
+return (str);
+}
