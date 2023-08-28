384104a078f3f7afe2e2eff067a179f907a11d87
reham-elsyed <rehamshipl666@gmail.com>
2023-08-28 12:35:42 -0700

    compare substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ **_strstr -function that compare two strings
+ *@haystack: input string
+ *@needle: input string
+ *Return: beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] > '\0'; i++)
+{
+for (j = 0; needle[j] > '\0'; j++)
+{
+if (haystack[i] == needle[j] && haystack[i-1] == ' ')
+{
+return (&(haystack[i]));
+}
+}
+}
+return ('\0');
+}
