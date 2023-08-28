73fcdc1714e944aeb79c4bdd6a12938af3e7a6dc
reham-elsyed <rehamshipl666@gmail.com>
2023-08-28 10:34:55 -0700

    string match

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,13 +10,13 @@ char *_strpbrk(char *s, char *accept)
 {
 int i, j;
 
-for (i = 0; i != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (s[i] == accept[j])
+if (accept[j] == s[i])
 {
-return ((&s[i]));
+return (&(s[i]));
 }
 }
 }
