db5a828cea83dd6514de1db34256ac890c8e5742
reham-elsyed <rehamshipl666@gmail.com>
2023-08-28 07:57:26 -0700

    match

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - search a string for any of a setof bytes
+ * @s: input value
+ * @accept: compare string
+ * Return: accept value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+for (i = 0; i != '\0'; i++)
+{
+for (j = 0; j != '\0'; j)
+{
+if (s[i] == accept[j])
+{
+return ((&s[i]));
+break;
+}
+}
+}
+return ('\0');
+}
