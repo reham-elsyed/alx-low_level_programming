52ad594639bc4d57152f3159494bbb529cbabf6a
reham-elsyed <rehamshipl666@gmail.com>
2023-08-28 12:39:43 -0700

    compare substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ for (i = 0; haystack[i] > '\0'; i++)
 {
 for (j = 0; needle[j] > '\0'; j++)
 {
-if (haystack[i] == needle[j] && haystack[i-1] == ' ')
+if (haystack[i] == needle[j] && haystack[i - 1] == ' ')
 {
 return (&(haystack[i]));
 }
