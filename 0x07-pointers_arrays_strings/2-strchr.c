6cd4da0d0b3e975b1fa4ffb9bd6a25694094a2b5
reham-elsyed <rehamshipl666@gmail.com>
2023-08-28 03:57:02 -0700

    return letter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strchr - function that return a letter
+ * @s: string of characters
+ * @c: input letter
+ * Return: s if found, null id not found
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; i != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return ('\0');
+}
