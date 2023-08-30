816b7c76c787d19934ee9f120d40408298e6a168
reham-elsyed <rehamshipl666@gmail.com>
2023-08-30 05:56:29 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - function to return factorial of a fiven number
+ * @n: imput number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * (factorial n - 1));
+	}
+}
