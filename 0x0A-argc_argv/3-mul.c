ee184f65af226067365afe7bc347ebc9675278f6
reham-elsyed <rehamshipl666@gmail.com>
2023-09-01 05:07:31 -0700

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- /**
-  * main - print sum
-  * @argc: num of cmd
-  * @argv:pointer of input array
-  * Return:0 success
-  */
+/**
+*main - print sum
+*@argc: num of cmd
+*@argv:pointer of input array
+*Return:0 success
+*/
 int main(int argc, char *argv[])
 {
 int res;
+
 if (argc == 3)
 {
-res = atoi(argv[1] * atoi(argv[2]);
-printf("%d\n", sum);
+res = atoi(argv[1]) * (atoi(argv[2]);
+printf("%d\n", res);
 }
 else
 {
