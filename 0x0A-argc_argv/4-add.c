227c9e3570f07dc68cf291302517638bc1766e38
reham-elsyed <rehamshipl666@gmail.com>
2023-09-01 06:09:19 -0700

    comment

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main - function 
- * @argc: input number
- * @argv: input array
- * Return: 0success
- */
+* main - function
+* @argc: input number
+* @argv: input array
+* Return: 0success
+*/
 int main(int argc, char *argv[])
 {
 int result = 0;
@@ -14,10 +14,14 @@ char *arr;
 
 while (--argc)
 {
-for (arr = argv[argc]; *arr; arr++)
+for (arr = (argv[argc]); *arr; arr++)
+{
 if (*arr < '0' || *arr > '9')
-return (printf("Error\n", 1);
+{
+return (printf("Error\n"), 1);
+}
 result += atoi(argv[argc]);
+}
 }
 printf("%d\n", result);
 return (0);
