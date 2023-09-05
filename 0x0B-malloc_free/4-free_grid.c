0e4edb8ae53262e50503fd1546b43e7794e7d2a4
reham-elsyed <rehamshipl666@gmail.com>
2023-09-05 04:42:54 -0700

    free grid allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * free_grid -function to free 2dgrid
+ * @grid: pointer to int pointer
+ * @height: input height
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+	free(grid[i]);
+free(grid);
+}
