ee35ae4f7c5e6e54bdf818e7986dc69063d8c199
reham-elsyed <rehamshipl666@gmail.com>
2023-09-13 09:29:50 -0700

    iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,17 +4,17 @@
  * array_iterator - function to iterate over array
  * @array: array input
  * @size: size of array
- * @action:input function 
+ * @action:input function
  *Return: nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-if (array && size > 0 && action)
+unsigned int i;
+if (array && size && action)
 {
 for (i = 0; i < size; i++)
 {
-action(array[i]);
+action(*array++);
 }
 }
 }
