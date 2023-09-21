d257122414ff9b253c53fd0a8b1196bd3a077b45
reham-elsyed <rehamshipl666@gmail.com>
2023-09-21 16:02:22 -0700

    printfitst

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void _printfirst(void) __attribute__ ((printfirst));
+/**
+ * printfirst - function to print line 
+ * @head: input
+ * @str: input string
+ * Return: nothing
+ */
+void printf(void)
+{
+printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
