1a59b59d3bb2591a65df65c7a55c11e4efa88d68
reham-elsyed <rehamshipl666@gmail.com>
2023-09-21 12:16:48 -0700

    node length

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length of a string
+ * @s: input value
+ * Return: length success
+ */
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen(s + 1));
+}
+/**
+ * print_list - print list
+ * @h: pointer 
+ *Return: size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{ printf([%d] %s\n, _strlen(h->str), h->str ? h ->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
