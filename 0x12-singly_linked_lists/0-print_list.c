93d10e4a55224d872654b2d6c6c4912f17a7c4cd
reham-elsyed <rehamshipl666@gmail.com>
2023-09-21 16:56:52 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -24,10 +24,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{ 
 		if (!h->str)
-			printf("[0] (nil)\n");
-		else 
 		{
-			printf("[%u]%s\n",_strlen(h->len), h->str);
+			printf("[0] (nil)\n");
+		}
+			else 
+		{
+			printf("[%u]%s\n",_strlen(h->str), h->str);
+		}
 		h = h->next;
 		n++;
 	}
