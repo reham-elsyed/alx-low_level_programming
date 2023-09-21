02b8cc742d9fb7d4b003838086cbbe05aee59444
reham-elsyed <rehamshipl666@gmail.com>
2023-09-21 15:04:24 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - gunction to free list
+ * @head: input head of node
+ * Return: number
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+
+	if (!head)
+	return;
+
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
+	
