d798af552fdb04ea32c8639718c32891b98985e5
reham-elsyed <rehamshipl666@gmail.com>
2023-09-22 07:30:54 -0700

    more strlen

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -35,3 +35,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 		return (new_node);
 }
+
+/**
+ * _strlen - function that return the length of a string
+ * @s: input value
+ * Return: length success
+ */
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen(s + 1));
+}
