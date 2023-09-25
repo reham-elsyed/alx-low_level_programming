9644567c658e49acef53d2c872e5719bc528f0f1
reham-elsyed <rehamshipl666@gmail.com>
2023-09-25 12:28:42 -0700

    delete fron start

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,7 +9,7 @@ int pop_listint(listint_t **head)
 	listint_t *current;
 	int num;
 
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 		return (0);
 		num = (*head)->n;
 		current = (*head)->next;
