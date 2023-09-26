9640de07ba25b949ccd9ac9cdedcb1ebb85620e0
reham-elsyed <rehamshipl666@gmail.com>
2023-09-25 23:06:13 -0700

    sum od data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - function to return sum of data
+ * @head: input pointer to list
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *current;
+	int sum = 0;
+
+	current = head;
+	if (!head)
+		return (0);
+	while (current != '\0')
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
