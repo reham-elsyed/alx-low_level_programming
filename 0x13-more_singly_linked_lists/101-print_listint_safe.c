b6ff3a2b5a16439a27e6bc101b139a23be4d5ba6
reham-elsyed <rehamshipl666@gmail.com>
2023-09-26 12:49:10 -0700

    just print already

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,37 @@
 #include "lists.h"
+/**
+ *print_listint_safe - function to print
+ *@head: pointer
+*Return: count
+*/
+
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
-	size_t count = 0;
-	size_t i;
-	const listint_t *printed_nodes[1024];
+const listint_t *current;
+size_t count = 0;
+size_t i;
+const listint_t *printed_nodes[1024];
 
-	if (!head)
-	{
-	exit(98);
-	}
-	current = head;
+if (!head)
+{
+exit(98);
+}
+current = head;
 
-	while (current != NULL)
-	{
-		for (i = 0; i < count; i++)
-		{
-		 if (printed_nodes[i] == current)
-		 {
-			 printf("[%p] %d\n", (void *)current, current->n);
-			  return (count);
-		 }
-		}
-		printed_nodes[count++] = current;
-		printf("[%p] ", (void *)current);
-		printf("%d\n", current->n);
-		current = current->next;
-	}
-	return (count);
+while (current != NULL)
+{
+for (i = 0; i < count; i++)
+{
+if (printed_nodes[i] == current)
+{
+printf("[%p] %d\n", (void *)current, current->n);
+return (count);
+}
+}
+printed_nodes[count++] = current;
+printf("[%p] ", (void *)current);
+printf("%d\n", current->n);
+current = current->next;
+}
+return (count);
 }
