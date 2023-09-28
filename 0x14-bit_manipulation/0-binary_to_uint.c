ea9f4416fec9e0e8a8ac63d2291360912328041d
reham-elsyed <rehamshipl666@gmail.com>
2023-09-28 06:12:47 -0700

    pit man

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0;
- int i = 0;
+unsigned int total = 0;
+int i = 0;
 
-	while (b[i] != '\0')
-	{
-		if (b[i] < '0' || b[i] > '1')
-		{
-			return (0);
-		}
-		total = 2 * total + (b[i] - '0');
+if (!b)
+return (0);
+while (b[i] != '\0')
+{
+if (b[i] < '0' || b[i] > '1')
+{
+return (0);
+}
+total = 2 * total + (b[i] - '0');
 i++;
-	}
-	return (total);
+}
+return (total);
 }
