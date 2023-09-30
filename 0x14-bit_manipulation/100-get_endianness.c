268ab0e847456c97e66bffeae9068fdd2c42d69f
reham-elsyed <rehamshipl666@gmail.com>
2023-09-30 01:49:16 -0700

    big emdian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,19 +1,10 @@
 #include "main.h"
+#include <endian.h>
 /**
  *get_endianness- function to check for big or little endian
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	int num;
-	char *c = (char *) &num;
-
-	if (*c)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+return (__BYTE_ORDER == __LITTLE_ENDIAN) ? 1 : 0;
 }
