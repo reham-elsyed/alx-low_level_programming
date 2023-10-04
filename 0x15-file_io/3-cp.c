8c40e46c02de219a837a51299d25ceb06358528d
reham-elsyed <rehamshipl666@gmail.com>
2023-10-04 09:20:37 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,6 @@
  */
 int main(int ac, char *argv[])
 {
-	const char *file_from;
 	int fp1, fp2;
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE];
@@ -22,20 +21,20 @@ if (ac != 3)
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
-file_from = argv[1];
-fp1 = open(file_from, O_RDONLY);
+fp1 = open(argv[1], O_RDONLY);
 if (fp1 == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fp2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
- if (fp2 == -1)
+fp2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+if (fp2 == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-	exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+exit(99);
 }
-while ((bytes = read(fp1, buf, sizeof(buf))) > 0)
+bytes = read(fp1, buf, sizeof(buf));
+while (bytes > 0)
 {
 if (write(fp2, buf, bytes) == -1)
 {
@@ -45,7 +44,7 @@ exit(99);
 }
 if (bytes == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 if (close(fp1) == -1 || close(fp2) == -1)
