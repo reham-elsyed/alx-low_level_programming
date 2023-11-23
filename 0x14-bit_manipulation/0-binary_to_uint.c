a2c9e2c004911410bd5af19575ae66b98d26a83e
reham-elsyed <rehamshipl666@gmail.com>
2023-11-23 12:16:46 -0800

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * binary_to_uint - function to convert binary numbers to unsigned int
+ * @b: pointer input to string of binary chars
+ * Return: 0 if input is not 0 or 1 else return converted-num
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = _strlen(b);
+	unsigned int total = 0;
+	unsigned int decval = 1;
+	int i;
+
+	if (!b)
+		return (0);
+	for (i = (len - 1); i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
+	}
+	return (total);
+}
+
+/**
+ * _strlen - function that return the length of a string
+ * @s: input value
+ * Return: length success
+ */
+int _strlen(const char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen(s + 1));
+}
