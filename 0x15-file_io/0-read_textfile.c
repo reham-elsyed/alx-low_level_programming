32fb5053b7a1427f65c2764ff3562c17393ed7ec
reham-elsyed <rehamshipl666@gmail.com>
2023-11-27 12:41:03 -0800

    iofiles

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,13 +11,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int pd;
 	ssize_t numbytes;
-	char buff[READ_BUF_SIZE *8];
+	char buff[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
 
 	pd = open(filename, O_RDONLY);
-		if(pd == -1)
+		if (pd == -1)
 			return (0);
 	numbytes = read(pd, &buff[0], letters);
 	numbytes = write(STDOUT_FILENO, &buff[0], numbytes);
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strlen -function to count length
+ * @c: input string
+ * Return: len
+ */
+int _strlen(char *c)
+{
+	int i = 0;
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+
+/**
+ * creat_file -function reads text file and prints it
+ * @filename: input file
+ * @text_content: input letters
+ * Return: 0 in succecsss
+ */
+
+ssize_t read_textfile(const char *filename, char *text_content)
+{
+        int pd;
+        ssize_t numbytes = 0;
+        ssize_t len = _strlen(text_content)
+
+        if (!filename)
+		return (-1);
+
+        pd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                if (pd == -1)
+                        return (-1);
+		if (len)
+	numbytes = write(pd, text_content, len);
+        close(pd);
+        return (numbytes == len ? 1 : -1);
+}
