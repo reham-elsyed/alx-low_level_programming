8ed4a0517824bb9f19d224b3e7d2954b996f51fb
reham-elsyed <rehamshepl666@gmail.com>
2024-02-01 15:53:23 -0800

    key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,9 @@
 #include "hash_tables.h"
 /**
- *
- *
+ *key_index - function to create key using hash function
+ *@key: input key
+ *@size: size of table
+ *Return: new_key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
