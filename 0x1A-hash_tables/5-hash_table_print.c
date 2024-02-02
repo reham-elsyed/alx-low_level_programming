98d0cd999afd91362990160d16bdb05f4f3edc18
reham-elsyed <rehamshepl666@gmail.com>
2024-02-02 08:15:30 -0800

    getter

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print- function to print hash table
+ *@ht: input table
+ *Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+
+	current = ht->array[i];
+
+	while (current != NULL)
+	{
+		printf("'%s': '%s'", current->key, current->value);
+		current = current->next;
+		if (current != NULL)
+		{
+			printf(", ");
+	}
+	}
+	}
+	printf("}\n");
+}
