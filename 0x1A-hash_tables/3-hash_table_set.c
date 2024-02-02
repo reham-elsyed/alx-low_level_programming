c8ca9c3d3d9182c93c9450f8c273b68449541212
reham-elsyed <rehamshepl666@gmail.com>
2024-02-02 06:45:32 -0800

    create node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_set -function to create and insert node in ht
+ *hash_table_set-function to create and insert node in ht
  *@ht: hash table array
  *@key: key
  *@value: value
- *Return : 1 on success
+ *Return:1 on success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -16,24 +16,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 	return (0);
-
 	new_value = malloc(strlen(value) + 1);
-
 	if (new_value == NULL)
 	{
 		return (0);
 	}
 	strcpy(new_value, value);
 	index = key_index((const unsigned char *)key, ht->size);
-
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(new_value);
 		return (0);
 	}
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
@@ -44,14 +39,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->next = NULL;
 
 	if (ht->array[index] == NULL)
-	{
 		ht->array[index] = new_node;
-	}
 	else
 	{
 		temp = ht->array[index];
 		new_node->next = temp;
 		ht->array[index] = new_node;
 	}
-	return 1;
+	return (1);
 }
