0ca7488da4e0d27387a6b4e500ba71e96724b0c0
reham-elsyed <rehamshepl666@gmail.com>
2024-02-02 07:37:22 -0800

    getter

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_get- function to get value of nodes
+ *@ht: input hadh table
+ *@key: input key
+ *Return: value assiciated with key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int = index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+
+		return (current->value);
+		current = current->next;
+	}
+		return (NULL);
+}
