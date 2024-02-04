41a92d04f3c9297668730b551434378bbd971ee9
reham-elsyed <rehamshepl666@gmail.com>
2024-02-04 15:11:14 -0800

    print hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *current;
+	char flag = 0;
 	if (ht == NULL)
 	{
 		return;
@@ -22,11 +23,11 @@ void hash_table_print(const hash_table_t *ht)
 
 	while (current != NULL)
 	{
-		printf("'%s': '%s'", current->key, current->value);
-		current = current->next;
-		if (current != NULL)
-	
+		if (flag == 1)
 			printf(", ");
+		printf("'%s': '%s'", current->key, current->value);
+		flag = 1;
+		current = current->next;
 	
 	}
 	}
