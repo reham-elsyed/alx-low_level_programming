6eef72fff48ccd9d4dc646ae7110b549a75c9bd0
reham-elsyed <rehamshepl666@gmail.com>
2024-05-06 03:16:54 -0700

    p

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ *linear_search -function to apply linear search
+ *@array: input pointer to array
+ *@size: size of the array
+ *@value: value of the integer
+ *Return: -1 if failure index of integer if found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t n = 0;
+	size_t i;
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			n = i;
+			break;
+		}
+		}
+		if (n){
+			return (n);
+		}
+		else
+		{
+			return (-1);
+		}
+
+}	
